18135befd2a796a568f6f4b628e492c1bd637c7a
weikai <weikai@ibears.io>
2025-02-25 01:06:39 +0800

    Test the options api in allow origin

--- error-handler.js
+++ error-handler.js
@@ -1,6 +1,6 @@
 const headers = {
 	'Content-Type': 'application/json',
-	'Allow-Control-Allow-Origin': '*',
+	'Allow-Control-Allow-Origin': 'http://localhost:3000',
 	'Allow-Control-Allow-Method': 'GET, OPTIONS, POST, DELETE, PATCH',
 	'Allow-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
 };
